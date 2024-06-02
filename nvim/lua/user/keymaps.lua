c66599e2db92377914e22d4eba93c1b3517fc77d
Pantelis Karatzis <p.karatzis@aftersalespro.gr>
2024-06-02 13:01:06 +0300

    plugins

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,15 +1,16 @@
-vim.g.mapleader = '\x1f' --ctrl
-vim.g.maplocalleader = '\x1f'
+-- Space is my leader.
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, noremap = true })
-vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, noremap = true })
+-- When text is wrapped, move by terminal rows, not lines, unless a count is provided.
+vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
 -- Reselect visual selection after indenting.
 vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true })
 vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true })
 
 -- Maintain the cursor position when yanking a visual selection.
--- http://ddrscott.github.io/blog/2016/yank-without-jank/
 vim.api.nvim_set_keymap('v', 'y', 'myy`y', { noremap = true })
 
 -- Disable annoying command line typo.
@@ -23,4 +24,15 @@ vim.api.nvim_set_keymap('i', ';;', '<Esc>A;', { noremap = true })
 vim.api.nvim_set_keymap('i', ',,', '<Esc>A,', { noremap = true })
 
 -- Quickly clear search highlighting.
-vim.api.nvim_set_keymap('n', '<Leader>k', ':nohlsearch<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>k', ':nohlsearch<CR>', { noremap = true })
+
+-- Open the current file in the default program.
+--vim.api.nvim_set_keymap('n', '<Leader>x', ':!xdg-open %<CR><CR>', { noremap = true })
+
+-- Move lines up and down.
+vim.api.nvim_set_keymap('i', '<A-j>', '<Esc>:move .+1<CR>==gi', { noremap = true })
+vim.api.nvim_set_keymap('i', '<A-k>', '<Esc>:move .-2<CR>==gi', { noremap = true })
+vim.api.nvim_set_keymap('n', '<A-j>', ':move .+1<CR>==', { noremap = true })
+vim.api.nvim_set_keymap('n', '<A-k>', ':move .-2<CR>==', { noremap = true })
+vim.api.nvim_set_keymap('v', '<A-j>', ":move '>+1<CR>gv=gv", { noremap = true })
+vim.api.nvim_set_keymap('v', '<A-k>', ":move '<-2<CR>gv=gv", { noremap = true })
